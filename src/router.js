ff88aa20eed236707e857c375166051b5763519b
Peter Dannemann <peter.dannemann@phila.gov>
2019-01-14 16:52:48 -0500

    Update router.js to work with github pages

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ Vue.use(Router);
 
 export default new Router({
   mode: "history",
+  base: process.env.NODE_ENV === "production" ? "/li-contractor-lookup/" : "/",
   routes: [
     {
       path: "/",
