0291d6bde9f63afb9d9facd48603098633bf4465
Peter Dannemann <peter.dannemann@phila.gov>
2019-01-14 16:04:11 -0500

    Fix href on index.html

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# deploy to https://cityofphiladelphia.github.io/li-contractor-lookup
+git push -f https://github.com/cityofphiladelphia/li-contractor-lookup.git master:gh-pages
+
+cd -
