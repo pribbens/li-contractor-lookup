8d0795b1417597a8e7e4e718f8963d49b592647c
Peter Dannemann <peter.dannemann@phila.gov>
2019-01-15 11:46:45 -0500

    Add background coloring to Home

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div class="outer-container">
+    <div class="app-container" id="app">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -10,36 +12,40 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.outer-container {
+  background-color: #f0f0f0;
+}
+
 @media (min-width: 400px) {
-  #app {
+  .app-container {
     width: 100%;
   }
 }
 
 @media (min-width: 550px) {
-  #app {
+  .app-container {
     width: 100%;
   }
 }
 
 @media (min-width: 1100px) {
-  #app {
-    width: 80%;
-    margin: auto;
-  }
-}
-
-@media (min-width: 1550px) {
-  #app {
+  .app-container {
     width: 70%;
     margin: auto;
   }
 }
 
-@media (min-width: 1600px) {
-  #app {
+@media (min-width: 1650px) {
+  .app-container {
     width: 60%;
+    margin: auto;
+  }
+}
+
+@media (min-width: 1700px) {
+  .app-container {
+    width: 40%;
     margin: auto;
   }
 }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div class="container greeting text-center">
-      <p>Contractors can use this tool to look up the statuses of their active permits.</p>
-      <p>
+    <div class="card-divider text-center">
+      <h5>Contractors can use this tool to look up the statuses of their active permits.</h5>
+      <h5>
         <b>NOTE:</b> Actions made in the last 24 hours may not be
         reflected on site
-      </p>
+      </h5>
     </div>
     <appTable class="table"></appTable>
   </div>
@@ -23,20 +23,57 @@ export default {
 </script>
 
 <style>
-.greeting {
-  padding-top: 2em;
-  width: 50%;
-  margin: auto;
+.card-divider {
+  background-color: rgb(197, 197, 197);
 }
 
 .table {
+  background-color: white;
+  padding-left: 1em;
+  padding-right: 1em;
   padding-bottom: 4em;
 }
 
-@media (min-width: 1350px) {
+@media (min-width: 400px) {
   .table {
-    width: 90%;
+    padding-left: 0em;
+    padding-right: 0em;
+  }
+}
+
+@media (min-width: 550px) {
+  .table {
+    padding-left: 0em;
+    padding-right: 0em;
+  }
+}
+
+@media (min-width: 1100px) {
+  .app-container {
+    padding-left: 1em;
+    padding-right: 1em;
+  }
+}
+
+@media (min-width: 1350px) {
+  .app-container {
     margin: auto;
+    padding-left: 1em;
+    padding-right: 1em;
+  }
+}
+
+@media (min-width: 1550px) {
+  .app-container {
+    padding-left: 1em;
+    padding-right: 1em;
+  }
+}
+
+@media (min-width: 1600px) {
+  .app-container {
+    padding-left: 1em;
+    padding-right: 1em;
   }
 }
 </style>
